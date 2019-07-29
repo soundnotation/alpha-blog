c7fee370365422d60e535a200344472db3d28a99
soundnotation <info@soundnotation.com>
2019-07-29 14:03:09 +0000

    changed article comp add timpestma

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,14 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import Timestamp from "react-timestamp"
+
 class Article extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <div class="article-title">
+        <div className="article-title">
           <a href={this.props.path}>{this.props.title}</a>
         </div>
-        <div class="article-body">
+        <div className="article-body">
           {this.props.description}
+          <div className="article-meta-details">
+            <small>
+              Created by: {this.props.author},
+              &nbsp;
+              <Timestamp relative date={this.props.created_at} autoUpdate />,
+              last updated: <Timestamp relative date={this.props.updated_at} autoUpdate />
+            </small>
+          </div>
         </div>
       </React.Fragment>
     );
@@ -18,6 +29,9 @@ class Article extends React.Component {
 Article.propTypes = {
   title: PropTypes.string,
   path: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
 };
 export default Article
